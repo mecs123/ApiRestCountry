e268d0c7f642d8a42ae6b8f5d7b0ae74517a32ea
ManuelCastro <manolo.9315@hotmail.com>
2024-11-13 10:58:19 -0500

    First commit

---
+++ src/main/java/com/RestCountry/restCountries/RestCountriesApplication.java
@@ -0,0 +1,13 @@
+package com.RestCountry.restCountries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCountriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCountriesApplication.class, args);
+	}
+
+}
